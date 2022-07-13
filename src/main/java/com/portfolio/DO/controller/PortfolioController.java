dd41312f215c33ea9f2279332cacef6843227b99
Diego Otranto <diegui98@live.com.ar>
2022-07-13 02:06:08 -0300

    cross origin fix

--- src/main/java/com/portfolio/DO/controller/PortfolioController.java
+++ src/main/java/com/portfolio/DO/controller/PortfolioController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://hosting-primer-portfolio.web.app/")
 public class PortfolioController {
     
     @Autowired
